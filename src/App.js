cd7f3da4f064a7b40c47b6eb9d0472a512918ead
Mandhouj A <nouitou@yahoo.fr>
2022-04-26 10:30:26 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Error } from './pages/Error';
+import { CreateEmployee } from './pages/CreateEmployee';
+import { EmployeeList } from './pages/EmployeeList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="*" element={<Error />} />
+      <Route path="/" index element={<CreateEmployee />} />
+      <Route path="/employee-list" element={<EmployeeList />} />
+    </Routes>
   );
 }
 
